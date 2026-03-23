583806de991280128a8a91bf41c87b2ee6f97a15
Aditya Bavadekar <bvkraditya@gmail.com>
2026-03-23 22:25:02 +0530

    chore: minor update

--- lock_script.sh
+++ lock_script.sh
@@ -7,6 +7,6 @@ echo "ALERT LOCK ON" | espeak-ng
 
 cd ~/random.sh/input-locker
 source .venv/bin/activate
-python input_lock.py > ~/input_lock.log 2>&1 &
+python input_lock.py >~/input_lock.log 2>&1
 
 echo "[ LOCKDOWN ] input restored"
