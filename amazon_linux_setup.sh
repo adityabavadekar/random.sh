fa3cfa43cf43ea09796b44e50220e4d2868585a7
AdityaBavadekar <bvkraditya@gmail.com>
2026-06-07 09:32:48 +0000

    amazon_linux: add nvim install

--- amazon_linux_setup.sh
+++ amazon_linux_setup.sh
@@ -24,6 +24,14 @@ chmod +x neofetch
 
 echo "[@@] neofetch installed run it using './neofetch'"  
 
+echo "[@@] Installing nvim"
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >> ~/.bashrc
+source ~/.bashrc
+echo "[@@] nvim installed"
+
 echo "[@@] Installing 'npm'"
 
 curl -fsSL https://rpm.nodesource.com/setup_22.x | sudo bash -
